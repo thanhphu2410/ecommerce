6cb0eb69a8df0b7d6994c120a9b5451e06aeeeac
Phú Trần <thanhphu2410@gmail.com>
2021-05-03 23:29:31 +0700

    testing heroku

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -14,6 +14,7 @@ class ShopController extends Controller
 {
     public function index()
     {
+        return session()->all();
         $products = $this->filter();
         $categories = Category::all();
         $subCategories = SubCategory::all();
